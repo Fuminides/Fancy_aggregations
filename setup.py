3f1b621525939cc4e2532e5d2de72a37f30cfc16
Javier Fumanal Idocin <Fuminides@users.noreply.github.com>
2019-12-17 17:04:45 +0100

    Setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='Fancy_aggregations',
+   version='1.0',
+   description='A collection of aggregations, such as OWAs, Choquet and Sugeno integrals, etc.',
+   author='Javier Fumanal Idocin',
+   author_email='javier.fumanal@unavarra.es',
+   packages=['Fancy_aggregations'],  #same as name
+   install_requires=['numpy'], #external packages as dependencies
+)
