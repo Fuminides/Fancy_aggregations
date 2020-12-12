2505fa31ca8d90079cceb3affd9aa779e9fdb18b
Javier Fumanal Idocin <Fuminides@users.noreply.github.com>
2020-12-12 13:15:42 +0100

    Update averaging_tnorms.py

--- Fancy_aggregations/averaging_tnorms.py
+++ Fancy_aggregations/averaging_tnorms.py
@@ -11,13 +11,21 @@ To suggest changes or submit new code please use the github page.
 
 import numpy as np
 
-def averaging_operator_min_delta_1d(x, tnorm):
+def averaging_operator_min_delta_1d(x, tnorm, purge_0=True):
 	
-	arg_min_x = np.argmin(x)  
-	min_x = x[arg_min_x]
-	x_1_n = x[np.arange(len(x))!=arg_min_x]
+	if purge_0:
+		x = np.array([y for y in x if y > 0])
 
-	return min_x + tnorm(x_1_n)
+	min_x = np.min(x)  
+	
+	x_1_n = x[x!=min_x]
+
+	if len(x_1_n) == 0:
+		t_norm_res = 0.0
+	else:
+		t_norm_res = tnorm(x_1_n)
+
+	return min_x + t_norm_res
 
 
 def averaging_operator_max_delta_1d(x, tnorm):
