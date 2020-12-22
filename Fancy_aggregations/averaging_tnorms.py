c4ddaf72a0fa82c7c6dbcf56df518d8b463a4958
Javier Fumanal Idocin <Fuminides@users.noreply.github.com>
2020-12-22 11:54:33 +0100

    Update averaging_tnorms.py

--- Fancy_aggregations/averaging_tnorms.py
+++ Fancy_aggregations/averaging_tnorms.py
@@ -10,23 +10,66 @@ To suggest changes or submit new code please use the github page.
 
 import numpy as np
 
+def v_isclose(a, b, rel_tol=1e-9, abs_tol=0.0):
+    """
+    returns True if a is close in value to b. False otherwise
+    :param a: one of the values to be tested
+    :param b: the other value to be tested
+    :param rel_tol=1e-9: The relative tolerance -- the amount of error
+                         allowed, relative to the absolute value of the
+                         larger input values.
+    :param abs_tol=0.0: The minimum absolute tolerance level -- useful
+                        for comparisons to zero.
+    NOTES:
+    -inf, inf and NaN behave similarly to the IEEE 754 Standard. That
+    is, NaN is not close to anything, even itself. inf and -inf are
+    only close to themselves.
+    The function can be used with any type that supports comparison,
+    substratcion and multiplication, including Decimal, Fraction, and
+    Complex
+    Complex values are compared based on their absolute value.
+    See PEP-0485 for a detailed description
+    """
+
+    if np.mean(np.equal(a, b)) == 1:  # short-circuit exact equality
+        return np.ones(a.shape)
+
+    if rel_tol < 0.0 or abs_tol < 0.0:
+        raise ValueError('error tolerances must be non-negative')
+
+    diff = abs(b - a)
+    c1 = diff <= abs(rel_tol * b)
+    c2 = diff <= abs(rel_tol * a)
+    c3 = diff <= abs_tol
+
+    return np.maximum(np.maximum(c1, c2), c3)
+
 def _my_is_close(a, b):
+  '''
+  Computes isclose() from a naive perspective, and applying the math isclose() but in a vectorized way.
+  
+  '''	
   abs_dif = np.abs(a - b)
-  return np.less(abs_dif, 0.05)
+  c1 = np.less(abs_dif, 0.05)
+  c2 = v_isclose(a, b)
+
+  return np.maximum(c1, c2)
+
   
 def averaging_operator_min_delta_1d(x, tnorm=np.min, purge_0=True):
 	if purge_0:
-		x = np.array([y for y in x if not math.isclose(y, 0, abs_tol = 10e-10)])
+		x = _my_is_close(x, np.zeros(len(x)))
 
 		if len(x) == 0:
 				return 0.0
 
 	min_x = np.min(x)  
- 
-	x_1_n = x[~_my_is_close(x, min_x)]
+ 	aux = np.zeros(len(x)) + min_x
+	x_1_n = x[~_my_is_close(x, aux)]
 
 	if len(x_1_n) == 0:
 		t_norm_res = 0.0
+
 	elif len(x_1_n) == 1:
 		t_norm_res = x_1_n - min_x
 	else:
@@ -43,9 +86,22 @@ def averaging_operator_min_delta(x, axis, keepdims, tnorm=np.min, purge_0=True):
 	return res
 
 def averaging_operator_max_delta_1d(x, tnorm):
-	
-	arg_max_x = np.argmax(x)  
-	max_x = x[arg_max_x]
-	x_1_n = x[np.arange(len(x))!=arg_max_x]
+	if purge_0:
+		x = _my_is_close(x, np.zeros(len(x)))
 
-	return max_x - tnorm(x_1_n)
+		if len(x) == 0:
+				return 0.0
+
+	min_x = np.max(x)  
+ 	aux = np.zeros(len(x)) + min_x
+	x_1_n = x[~_my_is_close(x, aux)]
+
+	if len(x_1_n) == 0:
+		t_norm_res = 0.0
+
+	elif len(x_1_n) == 1:
+		t_norm_res = x_1_n - min_x
+	else:
+		t_norm_res = tnorm(x_1_n - min_x)
+
+	return np.max(x) - t_norm_res
